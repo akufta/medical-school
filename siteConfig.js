7dde76c9c1ea4ea1451495002b0843e94c7b094b
Akufta <akufta7@yahoo.com>
2020-10-23 14:28:20 -0500

    +fix

--- siteConfig.js
+++ siteConfig.js
@@ -1,8 +1,7 @@
 module.exports = {
   name: "Allison Kufta",
   shortName: "Allison Kufta",
-  description:
-    "An aspiring doctor interested in the intersection between science, service, and the humanities.",
+  description: "Creative portfolio of Allison Kufta.",
   url: "https://allisonkufta.netlify.app", // Domain of your site without prefix!
   prefix: "/",
   author: "Allison Kufta",
--- src/pages/about.js
+++ src/pages/about.js
@@ -21,73 +21,36 @@ const AboutPage = ({ data }, location) => {
       <article className="post-content page-template no-image">
         <div className="post-content-body">
           <h3 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-">
-            A highlight reel of my dedication to life-long learning, running,
-            art, and coffee.
+            About this site
           </h3>
-          Images that are not mine are sourced in the public domain
-          (Unsplash.com)
-          <h3 id="dynamic-styles"> Life-Long Learning</h3>
+
           <p>
-            I commit to life-long learning, both in and out of the classroom. In
-            medicine, offers an ever evolving body of knowledge.{" "}
+            I am a curious person and committed to life-long learning, both in
+            and out of the classroom.{" "}
           </p>
           <p>
-            In fact, creating this website involved learning how to use a new
-            command line tool and templating system.{" "}
+            Creating this website involved learning how to use a new command
+            line tool (the gatsby-cli) and a templating system. I enjoy coding
+            for fun because it involves an exciting mix of science and
+            creativiity.{" "}
           </p>
           <p>
             {" "}
             For other projects, I've used the Hugo Static Site Generator, but
-            this website is created using Gatsby, which works more as a
-            progressive web app.
+            this website is created using Gatsby, which has a slightly different
+            feature set and is based the React Javascript framework. There are
+            advantages to static-site generators, including security and speed.
             <img
               src={
                 "https://www.gatsbyjs.com/static/9a172caa0d7bafa251ede1162087bded/8f6f3/home-page_fast-to-build.jpg"
               }
             />
           </p>
+          <p> </p>
+          <p> </p>
+
+          <h3 id="dynamic-styles"> Powered by coffee</h3>
           <p>
-            {" "}
-            I enjoy learning about science and medicine, but I also love
-            learning about different perspectives encapsulated by humanities
-            subjects. I have a wide range of interests, from politics, to
-            exercise (especially running) physiology/nutrition, politics, social
-            justice issues, sleep, memory, anatomical variation, open source,
-            and art techniques.{" "}
-          </p>
-          <p>
-            {" "}
-            Art, be it illustration, photography, design, is an awesome field
-            that lets me combine my interests in science and the humanities. I
-            love that there is always something new to learn and experiment
-            with. It's a relaxing activity that allows me to express myself.
-            I've also been able to implement my artwork in my small business,
-            which is a website about art resources and tutorials. It also has a
-            strong community of artists, and we learn with each other.
-            <a href="https://kalispera.xyz/portfolio/">
-              {" "}
-              <img src={" https://i.imgur.com/H1h0xIN.jpg"} />
-            </a>
-            Click on the image preview for more artwork.{" "}
-          </p>
-          <h3 id="dynamic-styles"> Running</h3>
-          <p>
-            Running will always be a favorite activity, as it is a healthy
-            outlet for stress, a time to think, a time to have great
-            conversations with friends, as well as to stay in shape.{" "}
-          </p>
-          <p>
-            This sport also allows me to always have a new goal. I've achieved a
-            few milestones in my journey in running: 5K, half marathon, and the
-            full marathon I ran in Chicago for the awesome charity, St. Jude.
-            With the help of so many others, we raised over $1,800 to advance
-            children's cancer research.{" "}
-          </p>
-          <img src={"https://i.imgur.com/IAoGTB4.jpg"} />
-          <img src={"https://i.imgur.com/AxdGMAm.jpg"} />
-          <h3 id="dynamic-styles"> Coffee</h3>
-          <p>
-            This human is powered by coffee and naps.
             <img
               src={
                 " https://images.unsplash.com/photo-1497515114629-f71d768fd07c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1662&q=80"
